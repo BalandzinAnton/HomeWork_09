3d228ca9ad963ad6c16ebcab884c8e91bcbb24a1
Anton <balandzin2602@gmail.com>
2022-10-16 12:44:29 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+using static System.Console;
+Clear();
+Write("Введите занчение N: ");
+int number = int.Parse(ReadLine()!);
+NaturalNumbers(number);
+WriteLine();
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Write($"{num}  ");
+    NaturalNumbers(num - 1);
+}
